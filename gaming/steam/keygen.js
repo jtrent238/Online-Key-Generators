0fa76213dc3f38fba7a8ebac945050def01db634
Trent Patterson <jtrent238@outlook.com>
2016-10-04 18:18:49 -0400

    Update keygen.js

--- gaming/steam/keygen.js
+++ gaming/steam/keygen.js
@@ -1,16 +1,32 @@
-<!DOCTYPE html>
-<html>
-<center>
-<body>
-
-<h1>Steam Key Generator</h1>
-
-<p id="demo">[Your Number Here]</p>
-
-<button type="button" onclick="myFunction()">Give Me a Number</button>
-
-<script src="/Online-Key-Generators/gaming/steam/keygen.js"></script>
-
-</body>
-</center>
-</html>
+    var lines;
+    var randomNumber;
+    var lastRandomNumber;
+    
+    $(document.body).ready(function () {
+      
+      // load the keys from the server
+      $.ajax({
+        url: 'Online-Key-Generators/gaming/steam/steam_keys.txt'
+      }).done(function(content) {
+        
+        // normalize the line breaks, then split into lines
+        lines = content.replace(/\r\n|\r/g, '\n').trim().split('\n');
+        
+        // only set up the click handler if there were lines found
+        if (lines && lines.length) {
+          $('#showLine').on('click', function () {
+            // loop to prevent repeating the last random number
+            while (randomNumber === lastRandomNumber) {
+              randomNumber = parseInt(Math.random() * lines.length);
+              // check to prevent infinite loop
+              if (lines.length === 1) { break; }
+            }
+            // keep track of the last random number
+            lastRandomNumber = randomNumber;
+            
+            // show the corresponding line
+            $('#keys').text(lines[randomNumber]);
+          });
+        }
+      });
+    });
